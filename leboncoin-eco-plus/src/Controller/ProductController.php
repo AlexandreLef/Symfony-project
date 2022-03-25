f852293379e84be73dfa83405a3db5734675158b
Julien <jojos38000@hotmail.com>
2022-03-25 15:58:35 +0100

    Navbar

--- leboncoin-eco-plus/src/Controller/ProductController.php
+++ leboncoin-eco-plus/src/Controller/ProductController.php
@@ -2,18 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProductController extends AbstractController
-{
+class ProductController extends AbstractController {
     #[Route('/product/list', name: 'product_list')]
-    public function index(): Response
-    {
-        return $this->render('product/list.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+    public function list(ProductRepository $productRepository): Response {
+        $products = $productRepository->findAll();
+        return $this->render('product/list.html.twig', ['products' => $products]);
     }
 }
 
